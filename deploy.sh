9f60064cca3e7a0d3f88673892e55281e0711a3e
SoleRay <pein_zero@126.com>
2018-04-02 01:16:34 -0700

    modify step 2 comment

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ sudo apt-get install slapd
 #2.重新配置，输入下面这条命令时，弹出图形界面，进入人机交互，这条命令下面的注释，是人机交互的提示和建议项
 sudo dpkg-reconfigure slapd
 #Omit OpenLDAP server configuration? ... No
-#DNS domain name: ... debuntu.local        //根据自己需要输入，但必须和后面的相关地方保持一致
+#DNS domain name: ... soleray.local        //根据自己需要输入，但必须和后面的相关地方保持一致，例如第三步的命令
 #Name of your organization: ... Whatever & Co
 #Admin Password: XXXXX
 #Confirm Password: XXXXX
